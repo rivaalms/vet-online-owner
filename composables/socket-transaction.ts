626c0443516398723a62dafbfcc6cb6a8665ba40
rivaalms <rvalms@icloud.com>
2024-05-12 21:51:55 +0700

    feat: socket notification handler

--- composables/socket-transaction.ts
+++ composables/socket-transaction.ts
@@ -13,4 +13,25 @@ export function useSocketConnection() {
    socket.on('disconnect', (reason) => {
       console.info('Socket disconnected:', reason)
    })
+
+   socket.on('notification', (message) => {
+      console.info('Socket notification:', message.id)
+      switch (message.id) {
+         case 'consultation-rejected':
+            onConsultationRejected(message)
+            break
+         default:
+            break
+      }
+   })
+}
+
+async function onConsultationRejected(message: any) {
+   const { data } = message
+   const consultationStore = useConsultationStore()
+
+   await showConsultation(data.consultation_id)
+      .then(resp => {
+         consultationStore.consultation = resp
+      })
 }
